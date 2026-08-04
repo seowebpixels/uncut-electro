1049cb4f5d0cf2ca7ad38f9ec9a99c92559af4d1
Marlan Pather <info@seowebpixels.com>
2026-08-04 22:54:08 +0200

    Add nodejs_compat flag to wrangler.jsonc and set static output mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: cloudflare(),
+
   vite: {
     plugins: [tailwindcss()]
   },
@@ -16,6 +18,4 @@ export default defineConfig({
   build: {
     inlineStylesheets: 'always',
   },
-
-  adapter: cloudflare(),
 });
